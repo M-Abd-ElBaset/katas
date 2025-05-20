8e06f5d6f25c96d1b6204660c7b1c4ca537cfa77
melgebaly <m.abdelbaset@silkysys.com>
2025-05-20 18:10:54 +0300

    completed Roman Numerals kata

--- src/RomanNumerals.php
+++ src/RomanNumerals.php
@@ -4,12 +4,36 @@ namespace App;
 
 class RomanNumerals
 {
-    public static function generate(int $number): string
+    const ROMAN_NUMERALS = [
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1
+    ];
+
+    public static function generate(int $number): string | bool
     {
-        if($number == 1){
-            return 'I';
+        if($number <= 0 || $number >= 4000){
+            return false;
         }
 
-        return 'II';
+        $result = '';
+        
+        foreach (self::ROMAN_NUMERALS as $numeral => $arabic) {
+            for (;$number >= $arabic; $number -= $arabic) {
+                $result .= $numeral;
+            }
+        }
+
+        return $result;
     }
 }
--- tests/RomanNumeralsTest.php
+++ tests/RomanNumeralsTest.php
@@ -7,15 +7,47 @@ class RomanNumeralsTest extends TestCase
 {
     /**
      * @test
+     * @dataProvider checks
      */
-    function it_generates_roman_numerals_for_1(){
-        $this->assertEquals('I', RomanNumerals::generate(1));
+    function it_generates_roman_numerals(int $number, string $expected){
+        $this->assertEquals($expected, RomanNumerals::generate($number));
     }
 
     /**
      * @test
      */
-    function it_generates_roman_numerals_for_2(){
-        $this->assertEquals('II', RomanNumerals::generate(2));
+    function it_cannot_generate_for_less_than_1(){
+        $this->assertFalse(RomanNumerals::generate(0));
+    }
+
+    /**
+     * @test
+     */
+    function it_cannot_generate_for_more_than_3999(){
+        $this->assertFalse(RomanNumerals::generate(0));
+    }
+
+    public function checks(){
+        return [
+            [1, 'I'],
+            [2, 'II'],
+            [3, 'III'],
+            [4, 'IV'],
+            [5, 'V'],
+            [6, 'VI'],
+            [7, 'VII'],
+            [8, 'VIII'],
+            [9, 'IX'],
+            [10, 'X'],
+            [40, 'XL'],
+            [50, 'L'],
+            [90, 'XC'],
+            [100, 'C'],
+            [400, 'CD'],
+            [500, 'D'],
+            [900, 'CM'],
+            [1000, 'M'],
+            [3999, 'MMMCMXCIX'],
+        ];
     }
 }
