0ec80f01982af4b8030ac2eec8370a069c369607
melgebaly <m.abdelbaset@silkysys.com>
2025-06-17 18:33:51 +0300

    finished refactoring Calculator add function

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -6,29 +6,44 @@ use Exception;
 
 class StringCalculator
 {
+    const MAX_NUMBER_ALLOWED = 1000;
+
+    protected string $delimiter = ",|\n";
+
     public function add(string $numbers)
     {
         if(!$numbers) {
             return 0;
         }
 
-        $delimiter = ",|\n";
+        $this->disallowNegatives($numbers = $this->parseString($numbers));
 
-        if (preg_match('/\/\/(.)\n/', $numbers, $matches)) {
-            $delimiter = $matches[1];
+        return array_sum($this->ignoreGreaterThanMax($numbers));
+    }
+
+    protected function parseString(string $numbers) : array
+    {
+        $customDelimiterPattern = "\/\/(.)\n";
+
+        if (preg_match("/{$customDelimiterPattern}/", $numbers, $matches)) {
+            $this->delimiter = $matches[1];
             $numbers = str_replace($matches[0], '', $numbers);
         }
 
-        $numbers = preg_split("/{$delimiter}/", $numbers);
+        return preg_split("/{$this->delimiter}/", $numbers);
+    }
 
+    protected function disallowNegatives(array $numbers) : void
+    {
         foreach ($numbers as $number) {
             if ($number < 0) {
                 throw new Exception("Negative numbers are not allowed.");
             }
         }
+    }
 
-        $numbers = array_filter($numbers, fn($number) => $number <= 1000);
-        
-        return array_sum($numbers);
+    protected function ignoreGreaterThanMax(array $numbers) : array
+    {
+        return array_filter($numbers, fn($number) => $number <= self::MAX_NUMBER_ALLOWED);
     }
 }
